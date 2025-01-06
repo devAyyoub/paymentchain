5707095771c6cda813b70b1b44e460aa10a89d2f
devAyyoub <ayyub2000@gmail.com>
2025-01-06 04:39:25 +0100

    added api gateway configuration

---
+++ paymentchainparent3/infraestructuradomain/apigateway/src/main/java/com/paymentchain/setups/GlobalPreFiltering.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.paymentchain.setups;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ *
+ * @author ayyoub
+ */
+
+@Slf4j
+@Component
+public class GlobalPreFiltering implements GlobalFilter{
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Global prefilter executed");
+        return chain.filter(exchange);
+    }
+    
+}
