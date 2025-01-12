1575bc44b76da875161612378267550329d8d036
devAyyoub <ayyub2000@gmail.com>
2025-01-12 04:25:53 +0100

    added OAUTH2 configuration with keycloak

--- paymentchainparent3/businessdomain/product/target/generated-sources/annotations/com/paymentchain/product/common/ProductResponseMapperImpl.java
+++ paymentchainparent3/businessdomain/product/target/generated-sources/annotations/com/paymentchain/product/common/ProductResponseMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-09T22:41:07+0100",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 23.0.1 (Homebrew)"
+    date = "2025-01-11T02:22:58+0100",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20241217-1506, environment: Java 17.0.13 (Eclipse Adoptium)"
 )
 @Component
 public class ProductResponseMapperImpl implements ProductResponseMapper {
