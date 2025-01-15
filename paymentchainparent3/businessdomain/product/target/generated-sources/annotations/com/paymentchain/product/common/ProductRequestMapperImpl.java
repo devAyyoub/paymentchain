94e2bdc152622b3afdbd0c8a1708b4b8fa3f9905
devAyyoub <ayyub2000@gmail.com>
2025-01-15 20:29:49 +0100

    Remove application.properties from version control

--- paymentchainparent3/businessdomain/product/target/generated-sources/annotations/com/paymentchain/product/common/ProductRequestMapperImpl.java
+++ paymentchainparent3/businessdomain/product/target/generated-sources/annotations/com/paymentchain/product/common/ProductRequestMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-14T20:37:09+0100",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 23.0.1 (Homebrew)"
+    date = "2025-01-15T20:26:08+0100",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20241217-1506, environment: Java 17.0.13 (Eclipse Adoptium)"
 )
 @Component
 public class ProductRequestMapperImpl implements ProductRequestMapper {
