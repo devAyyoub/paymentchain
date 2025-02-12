d09bdb9c67372967563af615e8bc480211c097e2
devAyyoub <ayyub2000@gmail.com>
2025-02-12 21:24:16 +0100

    FIXED Cors problems in order to connect with the front end

--- paymentchainparent3/businessdomain/product/target/generated-sources/annotations/com/paymentchain/product/common/ProductResponseMapperImpl.java
+++ paymentchainparent3/businessdomain/product/target/generated-sources/annotations/com/paymentchain/product/common/ProductResponseMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-02-12T21:03:36+0100",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 23.0.1 (Homebrew)"
+    date = "2025-02-12T21:23:58+0100",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.41.0.z20250115-2156, environment: Java 21.0.5 (Eclipse Adoptium)"
 )
 @Component
 public class ProductResponseMapperImpl implements ProductResponseMapper {
